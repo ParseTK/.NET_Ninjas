c33f6c1e44a3d66d1a2cc558ed163825faed335c
NeverRemeber9 <gabejr194@gmail.com>
2025-11-22 15:43:40 -0500

    Enhance Product model with additional properties
    Added properties for ProductId, ProductName, Email, and UnitPrice to the Product class.

--- SalesLedger/Models/Product.cs
+++ SalesLedger/Models/Product.cs
@@ -1,12 +1,21 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesLedger.Models
 {
     public class Product
     {
+       
+        public int ProductId { get; set; }
+
+        
+        public string ProductName { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 }
+
