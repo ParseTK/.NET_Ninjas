25340dd1f2b307666edfc3c22b470dd0faac61ea
NeverRemeber9 <gabejr194@gmail.com>
2025-11-22 16:19:13 -0500

    Add data annotations to Product properties
    Added the required key and placed it above its respective spot. Also, do you know if I keep the string email?

--- SalesLedger/Models/Product.cs
+++ SalesLedger/Models/Product.cs
@@ -5,14 +5,17 @@ namespace SalesLedger.Models
 {
     public class Product
     {
-       
+        [Key]
         public int ProductId { get; set; }
 
-        
+        [Required]
         public string ProductName { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public decimal UnitPrice { get; set; }
 
         public ICollection<Order> Orders { get; set; } = new List<Order>();
