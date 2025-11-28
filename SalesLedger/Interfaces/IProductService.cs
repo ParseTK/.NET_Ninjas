a22e4ea52df995aaa9761b6c5b8ca27696c08435
NeverRemeber9 <gabejr194@gmail.com>
2025-11-28 12:44:25 -0500

    Add methods to IProductService interface
    Added the Suggested Interface code. I would like for you to review it over.

--- SalesLedger/Interfaces/IProductService.cs
+++ SalesLedger/Interfaces/IProductService.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SalesLedger.Interfaces
 {
-    public interface IProductService
+       public interface IProductService
     {
+        Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default);
+        Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<bool> UpdateAsync(Product product, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
