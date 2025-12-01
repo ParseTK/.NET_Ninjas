c24a1d0c010269bace4120bd1b0ab8bd83d49336
ParseTK <tjkirchner2401@student.stcc.edu>
2025-12-01 16:19:13 -0500

    fixed id to guid in customers-models also removed some unneeded imports in other files

--- SalesLedger/Models/Customers.cs
+++ SalesLedger/Models/Customers.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 
 
@@ -12,17 +7,17 @@ namespace SalesLedger.Models
     public class Customers
     {
         [Key]
-        public int CustomerId { get; set; }
+        public Guid CustomerId { get; set; }
 
         [Required]
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
 
         [Required]
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
         [EmailAddress]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
-        public ICollection<Orders> Orders { get; set; } = new List<Orders>();
+        public ICollection<Orders> Orders { get; set; } = [];
     }
 }
