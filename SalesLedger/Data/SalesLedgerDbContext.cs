d765496c87b5edc1b6e36269bbfba426c97eb252
Nick Anastos <nanastos2401@student.stcc.edu>
2025-12-03 11:42:32 -0500

    Added fluent API configuration

--- SalesLedger/Data/SalesLedgerDbContext.cs
+++ SalesLedger/Data/SalesLedgerDbContext.cs
@@ -5,11 +5,48 @@ namespace SalesLedger.Data
 {
     public class SalesLedgerDbContext : DbContext
     {
-        public SalesLedgerDbContext(DbContextOptions<SalesLedgerDbContext> options) : 
-            base(options) { }
+        public SalesLedgerDbContext(DbContextOptions<SalesLedgerDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<Customers> Customers { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<Orders> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Products
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.Property(e => e.UnitPrice)
+                    .HasColumnType("decimal(10, 2)");
+            });
+            // Orders
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.Property(e => e.UnitPrice)
+                    .HasColumnType("decimal(10, 2)");
+
+                //relationship: Orders>Customers
+                entity.HasOne(e => e.Customer)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(e => e.CustomerId)
+                    .OnDelete(DeleteBehavior.Restrict); // Prevent accidental customer deletion if they have orders
+
+                //relationship: Orders>Products
+                entity.HasOne(e => e.Product)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(e => e.ProductId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                // added a index on email for faster lookup
+                entity.HasIndex(e => e.Email)
+                    .IsUnique();
+            });
+        }
     }
 }
